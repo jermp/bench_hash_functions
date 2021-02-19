2d747827b77489edba583336a6bc5cffbf850e64
Giulio Ermanno Pibiri <jeis90@gmail.com>
2021-02-19 11:48:00 +0100

    all static functions

--- include/hashers.hpp
+++ include/hashers.hpp
@@ -31,17 +31,17 @@ struct murmurhash2_64 {
         return "murmurhash2_64";
     }
 
-    uint64_t mod(hash_type h, uint64_t n) {
+    static uint64_t mod(hash_type h, uint64_t n) {
         return mod64(h, n);
     }
 
     // specialization for std::string
-    hash_type hash(std::string const& val, uint64_t seed) {
+    static hash_type hash(std::string const& val, uint64_t seed) {
         return MurmurHash64A(val.data(), val.size(), seed);
     }
 
     // specialization for uint64_t
-    hash_type hash(uint64_t val, uint64_t seed) {
+    static hash_type hash(uint64_t val, uint64_t seed) {
         return MurmurHash64A(&val, sizeof(val), seed);
     }
 };
@@ -53,19 +53,19 @@ struct murmurhash3_128 {
         return "murmurhash3_128";
     }
 
-    uint64_t mod(hash_type h, uint64_t n) {
+    static uint64_t mod(hash_type h, uint64_t n) {
         return mod128(h, n);
     }
 
     // specialization for std::string
-    hash_type hash(std::string const& val, uint64_t seed) {
+    static hash_type hash(std::string const& val, uint64_t seed) {
         hash_type out;
         MurmurHash3_x86_128(val.data(), val.size(), seed, &out);
         return out;
     }
 
     // specialization for uint64_t
-    hash_type hash(uint64_t val, uint64_t seed) {
+    static hash_type hash(uint64_t val, uint64_t seed) {
         hash_type out;
         MurmurHash3_x86_128(&val, sizeof(val), seed, &out);
         return out;
@@ -79,17 +79,17 @@ struct cityhash_64 {
         return "cityhash_64";
     }
 
-    uint64_t mod(hash_type h, uint64_t n) {
+    static uint64_t mod(hash_type h, uint64_t n) {
         return mod64(h, n);
     }
 
     // specialization for std::string
-    hash_type hash(std::string const& val, uint64_t seed) {
+    static hash_type hash(std::string const& val, uint64_t seed) {
         return CityHash64WithSeed(val.data(), val.size(), seed);
     }
 
     // specialization for uint64_t
-    hash_type hash(uint64_t val, uint64_t seed) {
+    static hash_type hash(uint64_t val, uint64_t seed) {
         return CityHash64WithSeed(reinterpret_cast<char const*>(&val), sizeof(val), seed);
     }
 };
@@ -101,12 +101,12 @@ struct cityhash_128 {
         return "cityhash_128";
     }
 
-    uint64_t mod(hash_type h, uint64_t n) {
+    static uint64_t mod(hash_type h, uint64_t n) {
         return mod128(h, n);
     }
 
     // specialization for std::string
-    hash_type hash(std::string const& val, uint64_t seed) {
+    static hash_type hash(std::string const& val, uint64_t seed) {
         auto ret = CityHash128WithSeed(val.data(), val.size(), {seed, seed});
         hash_type out;
         *(reinterpret_cast<uint64_t*>(&out) + 0) = ret.first;
@@ -115,7 +115,7 @@ struct cityhash_128 {
     }
 
     // specialization for uint64_t
-    hash_type hash(uint64_t val, uint64_t seed) {
+    static hash_type hash(uint64_t val, uint64_t seed) {
         auto ret =
             CityHash128WithSeed(reinterpret_cast<char const*>(&val), sizeof(val), {seed, seed});
         hash_type out;
@@ -132,17 +132,17 @@ struct spookyhash_64 {
         return "spookyhash_64";
     }
 
-    uint64_t mod(hash_type h, uint64_t n) {
+    static uint64_t mod(hash_type h, uint64_t n) {
         return mod64(h, n);
     }
 
     // specialization for std::string
-    hash_type hash(std::string const& val, uint64_t seed) {
+    static hash_type hash(std::string const& val, uint64_t seed) {
         return SpookyHash::Hash64(val.data(), val.size(), seed);
     }
 
     // specialization for uint64_t
-    hash_type hash(uint64_t val, uint64_t seed) {
+    static hash_type hash(uint64_t val, uint64_t seed) {
         return SpookyHash::Hash64(&val, sizeof(val), seed);
     }
 };
@@ -154,12 +154,12 @@ struct spookyhash_128 {
         return "spookyhash_128";
     }
 
-    uint64_t mod(hash_type h, uint64_t n) {
+    static uint64_t mod(hash_type h, uint64_t n) {
         return mod128(h, n);
     }
 
     // specialization for std::string
-    hash_type hash(std::string const& val, uint64_t seed) {
+    static hash_type hash(std::string const& val, uint64_t seed) {
         uint64_t h1 = seed, h2 = seed;
         SpookyHash::Hash128(val.data(), val.size(), &h1, &h2);
         hash_type out;
@@ -169,7 +169,7 @@ struct spookyhash_128 {
     }
 
     // specialization for uint64_t
-    hash_type hash(uint64_t val, uint64_t seed) {
+    static hash_type hash(uint64_t val, uint64_t seed) {
         uint64_t h1 = seed, h2 = seed;
         SpookyHash::Hash128(&val, sizeof(val), &h1, &h2);
         hash_type out;
